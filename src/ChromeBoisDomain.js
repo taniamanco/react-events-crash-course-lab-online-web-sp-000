97db05a33a52cb6b473b0587a3ded07e16c604b5
Tania Manco <belalov.tania16@gmail.com>
2020-01-08 19:56:56 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -8,7 +8,7 @@ export default class ChromeBoisDomain extends Component {
       drawChromeBoiAtCoords(event.clientX, event.clientY)
   }
 
-  keyHandler = (event) => {
+  resize = (event) => {
       if (event.key === 'a') {
           resize ('+')
       } else if (event.key === 's') {
@@ -21,7 +21,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas
         onMouseMove={this.handleMouseMove}
         onClick={() => {toggleCycling()}}
-        keyHandler = {this.keyHandler}
+        resize = {this.resize}
         width='900'
         height='600'
         tabIndex="0">
