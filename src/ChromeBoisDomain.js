9f37b413977f8b9a1288f088ae23ab09f0541277
Tania Manco <belalov.tania16@gmail.com>
2020-01-08 19:57:54 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -21,7 +21,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas
         onMouseMove={this.handleMouseMove}
         onClick={() => {toggleCycling()}}
-        resize = {this.resize}
+        onClick={() => {resize()}}
         width='900'
         height='600'
         tabIndex="0">
